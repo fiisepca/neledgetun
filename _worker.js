ab91568655605a2a7d1e7685bebcb212c7ff696f
fiisepca <fmpcs@proton.me>
2025-06-12 07:36:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = '0abe1078-1558-467b-9b56-90e22b44f75b';
+let proxyIP = 'nima.nscl.ir';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
