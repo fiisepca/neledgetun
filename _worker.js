0c56204aa9ec5a8c63d3fea1dbd81bb4f54c6da2
fiisepca <fmpcs@proton.me>
2025-06-26 08:52:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = '26de8045-037a-40ac-88a3-139a2aec635a';
+let proxyIP = 'nima.nscl.ir';
 let DNS64Server = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
